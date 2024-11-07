71f768b1d36466b6abe5cb4e82a1052cd961901f
shifna majeed <sshifnamajeed@gmail.com>
2024-11-07 16:28:32 +0530

    dsa updates

--- linked_list.js
+++ linked_list.js
@@ -1,4 +1,4 @@
-//^ 1. BASIC OPERATIONS
+//^^^ 1. BASIC OPERATIONS
 
 // Insert at beginning
 
@@ -162,22 +162,345 @@
 
 // Insert at a position
 
+// class Node {
+//     constructor(data) {
+//         this.data=data;
+//         this.next=null;
+//     }
+// }
+
+// class LinkedList {
+//     constructor() {
+//         this.head=null;
+//         this.tail=null
+//     }
+
+//     append(data){
+//         const newNode=new Node(data);
+//         if(this.head==null){
+//             this.head=newNode;
+//             this.tail=newNode;
+//         }else{
+//             this.tail.next=newNode;
+//             this.tail=newNode;
+//         }
+//     }
+
+//     insertBeginning(data){
+//                 const newNode= new Node(data);
+//                 newNode.next=this.head;
+//                 this.head=newNode;
+        
+//                 if(this.tail==null){
+//                     this.tail=newNode;
+//                 }
+//             }
+        
+//     insertAt(position, data) {
+//         const newNode= new Node(data);
+//         if(position===1){
+//             insertBeginning(data);
+//         }
+//         let current=this.head;
+//         let previous=null;
+//         let index=1;
+
+//         while (current!==null && index<position) {
+//             previous=current;
+//             current=current.next;
+//             index++;
+//         }
+
+//         newNode.next=current;
+//         if (previous !== null) {
+//         previous.next=newNode;
+//         }
+
+//         if(newNode.next==null){
+//             this.tail=newNode;
+//         }
+//     }
+    
+
+//     display(){
+//         if(this.head==null){
+//             console.log('no node occure')
+//         }else{
+//             let current=this.head;
+//             while (current !==null) {
+//                 console.log(current.data);
+//                 current=current.next;
+//             }
+//         }
+//     }
+// }
+
+// const list = new LinkedList();
+// list.append(5);
+// list.append(10);
+// list.append(20);
+// list.insertAt(3,15)
+// list.display();
+
+
+
+// Count nodes
+
+// class Node {
+//     constructor(data) {
+//         this.data=data;
+//         this.next=null;
+//     }
+// }
+
+// class LinkedList {
+//     constructor() {
+//         this.head=null;
+//         this.tail=null;
+//     }
+
+//     add(data){
+//         let newNode=new Node(data);
+//         if(this.head ==null){
+//             this.head=newNode;
+//             this.tail=newNode;
+//         }else{
+//           this.tail.next=newNode;
+//           this.tail=newNode;
+//         }
+//     }
+
+//     count(){
+//         let count=0;
+//         let current=this.head;
+//         while (current!=null) {
+//             current=current.next;
+//             count++;
+//         }
+//         console.log('total count: ',count);
+        
+//     }
+// }
+
+// let list=new LinkedList;
+// list.add(21);
+// list.add(31);
+// list.add(21);
+// list.add(31);
+// list.add(21);
+
+// list.count();
+
+
+
+// Find a Node
+
+// class Node {
+//     constructor(data) {
+//         this.data=data;
+//         this.next=null
+//     }
+// }
+
+// class LinkedList {
+//     constructor() {
+//         this.head=null;
+//         this.tail=null;
+//     }
+
+//     add(data){
+//         const newNode = new Node(data);
+//         if (this.head==null) {
+//             this.head=newNode;
+//             this.tail=newNode;
+//         }else{
+//             this.tail.next=newNode;
+//             this.tail=newNode;
+//         }
+//     }
+
+//     find(data){
+//         let current=this.head;
+//         while (current!==null && current.data!==data) {
+//             current=current.next;
+//         } 
+
+//         if(current!=null){
+//             console.log(current);
+//         }else{
+//             console.log('sorry, your node is not found');
+            
+//         }
+        
+//     }
+// }
+
+// let list = new LinkedList();
+// list.add(10);
+// list.add(30);
+// list.add(50);
+
+// list.find(10);
+
+
+// Delete a Node
+
+// class Node {
+//     constructor(data) {
+//         this.data=data;
+//         this.next=null
+//     }
+// }
+
+// class LinkedList {
+//     constructor() {
+//         this.head=null;
+//         this.tail=null;
+//     }
+
+//     add(data){
+//         const newNode = new Node(data);
+//         if (this.head==null) {
+//             this.head=newNode;
+//             this.tail=newNode;
+//         }else{
+//             this.tail.next=newNode;
+//             this.tail=newNode;
+//         }
+//     }
+
+//     delete(data){
+//         let current = this.head;
+//         let previous=null;
+        
+//     // Case when the node to delete is the head node
+//     if (current.data === data) {
+//         this.head = current.next;
+
+//         if (this.head === null) {
+//             this.tail = null;
+//         }
+//         return;
+//     }
+
+//         while (current!==null && current.data!==data) {
+//            previous=current
+//            current = current.next;
+//         }
+
+//         if(current==null){
+//             console.log('sorry , this node is not found');
+//         }else{
+//             previous.next=current.next;
+//         }
+//     }
+
+//     display(){
+//        let current=this.head;
+//         while (current!==null) {
+//             console.log(current);
+//             current=current.next;
+//         }
+//     }
+// }
+
+// let list = new LinkedList();
+// list.add(10);
+// list.add(30);
+// list.add(50);
+
+// list.delete(10);
+// list.display();
+
+
+
+
+//^^^ 2.INTERMEDIATE PROBLEMS
+
+
+// Reverse the linkedList
+
+// class Node {
+//     constructor(data) {
+//         this.data=data;
+//         this.next=null
+//     }
+// }
+
+// class LinkedList {
+//     constructor() {
+//         this.head=null;
+//         this.tail=null;
+//     }
+
+//     add(data){
+//         const newNode = new Node(data);
+//         if (this.head==null) {
+//             this.head=newNode;
+//             this.tail=newNode;
+//         }else{
+//             this.tail.next=newNode;
+//             this.tail=newNode;
+//         }
+//     }
+
+//     reverse(){
+//         let previous=null;
+//         let next=null;
+//         let current=this.head;
+//         this.tail=current;
+
+//         while (current!==null) {
+//             next=current.next;
+//             current.next=previous;
+
+//             previous=current;
+//             current=next;
+//         }
+//         this.head=previous;
+//     }
+
+//     display(){
+//        let current=this.head;
+//         while (current!==null) {
+//             console.log(current.data);
+//             current=current.next;
+//         }
+//     }
+// }
+
+// let list = new LinkedList();
+// list.add(10);
+// list.add(30);
+// list.add(50);
+
+// console.log('Original List');
+// list.display();
+
+// console.log('Reversed List');
+// list.reverse();
+// list.display();
+
+
+
+// Detect if there is a loop in a linkedList
+
 class Node {
     constructor(data) {
         this.data=data;
-        this.next=null;
+        this.next=null
     }
 }
 
 class LinkedList {
     constructor() {
         this.head=null;
-        this.tail=null
+        this.tail=null;
     }
 
-    append(data){
-        const newNode=new Node(data);
-        if(this.head==null){
+    add(data){
+        const newNode = new Node(data);
+        if (this.head==null) {
             this.head=newNode;
             this.tail=newNode;
         }else{
@@ -186,27 +509,28 @@ class LinkedList {
         }
     }
 
-    insertAt(position, data) {
-        
-    }
-    
+    hasLoop() {
+        let slow = this.head;
+        let fast = this.head;
 
-    display(){
-        if(this.head==null){
-            console.log('no node occure')
-        }else{
-            let current=this.head;
-            while (current !==null) {
-                console.log(current.data);
-                current=current.next;
-            }
+        while (fast !== null && fast.next !== null) {
+            slow = slow.next;          
+            fast = fast.next.next;     
+
+            if (slow === fast) {     // the fast is running faster than slow, so, when the slow and fast reach in the same node only it  will return true.    
+                return true;        //and only when it works in a cycle loop they will meet at a same node together. 
+            }                      //(it is not checking the data same or not, it checks if they are in the same node eppozhenkilum ee cycle il karangi nadakkumbo) 
         }
+        
+        return false;                    
     }
+
 }
 
-const list = new LinkedList();
-list.append(5);
-list.append(10);
-list.append(20);
-list.insertAt(2,15)
-list.display();
+let list = new LinkedList();
+list.add(10);
+list.add(30);
+list.add(50);
+list.tail.next=list.head.next;  //If you make the last node's next point to any previous node (instead of null), 
+console.log(list.hasLoop());   //it creates a loop (cycle).This means the list doesn't have an end and goes in circles.
+
